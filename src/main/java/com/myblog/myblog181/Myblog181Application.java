f39dcfed222fb8e7327f53756873f4baa5eef292
Pawan Vijay Tayde <pvt1504@gmail.com>
2024-01-04 17:28:08 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblog/myblog181/Myblog181Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog181;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog181Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog181Application.class, args);
+	}
+
+}
